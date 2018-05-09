d2d8b0bef8b2a6a903d1abf89de73469d11e5b94
zero24x <zero@you-spam.com>
2018-05-09 11:25:37 +0200

    qt5 spinner

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -101,9 +101,11 @@ private:
     TransactionView *transactionView;
     RPCConsole *rpcConsole;
 
-    QMovie *syncIconMovie;
-
     uint64_t nWeight;
+
+    /** Keep track of previous number of blocks, to detect progress */
+    int prevBlocks;
+    int spinnerFrame;
 
     /** Create the main UI actions. */
     void createActions();
